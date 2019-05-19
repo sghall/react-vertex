880ed113815353fc0312373323128be9c5de59a3
Steve Hall <hall.steven.g@gmail.com>
2019-05-19 10:54:45 -0700

    update demo

--- demos/AxesHelper/Torus.js
+++ demos/AxesHelper/Torus.js
@@ -4,11 +4,12 @@ import { useTexturedPhong } from '@react-vertex/material-hooks'
 import { useTorusElements } from '@react-vertex/geometry-hooks'
 import tilesDiffUrl from 'static/textures/tiles_pink_diff.png'
 
-const uVScale = [6.0, 3.0]
+const uVScale = [6.0, 1.0]
 
 function Torus() {
   const gl = useWebGLContext()
-  const program = useTexturedPhong(tilesDiffUrl)
+
+  const program = useTexturedPhong(tilesDiffUrl, 0.15, 500)
   useUniform2fv(gl, program, 'uVScale', uVScale)
 
   const torusElements = useTorusElements(10, 1, 16, 100)
