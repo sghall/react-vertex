67864ed4aaabaf59d691f148b59926433cad987f
Steve Hall <hall.steven.g@gmail.com>
2019-05-24 04:05:10 -0700

    update demo

--- demos/MaterialsExplorer/Scene.js
+++ demos/MaterialsExplorer/Scene.js
@@ -4,7 +4,7 @@ import { useOrbitCamera, useOrbitControls } from '@react-vertex/orbit-camera'
 import { useCanvasSize, useRender, usePointLight } from '@react-vertex/core'
 import { useHex } from '@react-vertex/color-hooks'
 import { Sphere, Torus, Cylinder, Box, Plane, Circle } from './geometries'
-import { SolidPhong, TexturedPhong, BasicSolid } from './materials'
+import { SolidPhong, TexturedPhong, BasicSolid, BasicTextured } from './materials'
 import tilesBlue from 'static/textures/tiles_blue_diff.png'
 import tilesPink from 'static/textures/tiles_pink_diff.png'
 import hexagons from 'static/textures/hexagons.jpg'
@@ -23,6 +23,7 @@ function PointLightScene() {
   const [lightPosition, setLightPostion] = useState([0, 0, 0])
 
   const { value: Material } = useSelectControl('Material: ', [
+    { value: BasicTextured, label: 'Basic Textured' },
     { value: BasicSolid, label: 'Basic Solid' },
     { value: TexturedPhong, label: 'Textured Phong' },
     { value: SolidPhong, label: 'Solid Phong' },
--- demos/MaterialsExplorer/materials/index.js
+++ demos/MaterialsExplorer/materials/index.js
@@ -1,3 +1,4 @@
 export { default as SolidPhong } from './SolidPhong'
 export { default as TexturedPhong } from './TexturedPhong'
 export { default as BasicSolid } from './BasicSolid'
+export { default as BasicTextured } from './BasicTextured'
