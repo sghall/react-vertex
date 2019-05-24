1bad9b471ab5c193dc63f165e72970e2c326c42b
Steve Hall <hall.steven.g@gmail.com>
2019-05-24 14:54:00 -0700

    tweak demo

--- demos/MaterialsExplorer/geometries/Cylinder.js
+++ demos/MaterialsExplorer/geometries/Cylinder.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react'
 import { useCylinderElements } from '@react-vertex/geometry-hooks'
 
 function Cylinder() {
-  const elements = useCylinderElements(10, 10, 15, 30, 30)
+  const elements = useCylinderElements(10, 10, 15, 50, 30)
   return <geometry {...elements} />
 }
 
--- demos/MaterialsExplorer/geometries/Sphere.js
+++ demos/MaterialsExplorer/geometries/Sphere.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react'
 import { useSphereElements } from '@react-vertex/geometry-hooks'
 
 function Sphere() {
-  const elements = useSphereElements(12, 30, 30)
+  const elements = useSphereElements(11, 35, 35)
   return <geometry {...elements} />
 }
 
