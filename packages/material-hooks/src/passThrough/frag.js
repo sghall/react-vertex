0605b4f68392d581d0af078ec690b6aa7d66e3ba
Steve Hall <hall.steven.g@gmail.com>
2019-05-29 15:56:51 -0700

    add pass through shaders

---
+++ packages/material-hooks/src/passThrough/frag.js
@@ -0,0 +1,13 @@
+
+export default `
+  precision <<FLOAT_PRECISION>> float;
+
+  uniform sampler2D texture;
+  uniform vec2 resolution;
+  
+  void main() {
+  	vec2 uv = gl_FragCoord.xy / resolution.xy;
+    
+    gl_FragColor = texture2D(texture, uv);
+  }
+`
