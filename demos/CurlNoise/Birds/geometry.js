d07445477d7cdc9aac0dc1e64018c775cb06b881
Steve Hall <hall.steven.g@gmail.com>
2019-05-31 12:56:53 -0700

    stub out curl noise demo

---
+++ demos/CurlNoise/Birds/geometry.js
@@ -0,0 +1,46 @@
+export default function birdGeometry(size) {
+  const instanceCount = size * size
+
+  const w = 30
+  const d = 12
+
+  const vertType1 = 1
+  const vertType2 = 2
+  const vertType3 = 3
+
+  // prettier-ignore
+  const vertices = [
+    // BODY
+    0, 0, -20, vertType1, 0, -6, -20, vertType1, 0, 0, 25, vertType1,
+  
+    // LEFT WING
+    0, 0, +d, vertType1, 0, 0, -d, vertType1, -w, 0, -d, vertType2,
+    -w, 0, +d, vertType2, 0, 0, d, vertType1, -w, 0, -d, vertType2,
+    -w, 0, -d, vertType2, -w * 1.5, 0, -d, vertType3, -w, 0, +d, vertType2,
+    
+    // RIGHT WING
+    0, 0, -d, vertType1, 0, 0, +d, vertType1, +w, 0, -d, vertType2, 
+    +w, 0, -d, vertType2, 0, 0, d, vertType1, +w, 0, +d, vertType2,
+    +w, 0, +d, vertType2, +w * 1.5, 0, -d, vertType3, +w, 0, -d, vertType2,
+  ]
+
+  // prettier-ignore
+  const indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+  const colors = []
+  const uvs = []
+
+  for (let i = 0; i < instanceCount; i++) {
+    const x = (i % size) / size
+    const y = Math.floor(i / size) / size
+
+    colors[i * 3 + 0] = Math.max(x, 1 - x)
+    colors[i * 3 + 1] = Math.max(y, 1 - y)
+    colors[i * 3 + 2] = 1
+
+    uvs[i * 2 + 0] = x
+    uvs[i * 2 + 1] = y
+  }
+
+  return { instanceCount, indices, vertices, colors, uvs }
+}
---
+++ demos/CurlNoise/Scene.js
@@ -0,0 +1,46 @@
+import React, { memo, useState, useEffect } from 'react'
+import { timer } from 'd3-timer'
+import { useOrbitCamera, useOrbitControls } from '@react-vertex/orbit-camera'
+import {
+  useCanvasSize,
+  useRender,
+} from '@react-vertex/core'
+import { useDataTextures } from './PingPong'
+import Birds from './Birds'
+
+function Scene() {
+  const { width, height } = useCanvasSize()
+  const renderScene = useRender()
+
+  const [elapsed, setElapsed] = useState(0)
+
+  const camera = useOrbitCamera(55, width / height, 1, 5000, c => {
+    c.setPosition([0, 0, 500])
+  })
+  useOrbitControls(camera)
+
+  const [texPosition, texVelocity] = useDataTextures(16)
+
+  useEffect(() => {
+    const timerLoop = timer(e => {
+      renderScene()
+      setElapsed(e)
+    })
+
+    return () => timerLoop.stop()
+  }, [renderScene])
+
+  return (
+    <camera view={camera.view} projection={camera.projection}>
+      <Birds
+        elapsed={elapsed}
+        texPosition={texPosition}
+        texVelocity={texVelocity}
+      />
+    </camera>
+  )
+}
+
+Scene.propTypes = {}
+
+export default memo(Scene)
