487739e70c61a93eb77e2551521e450896d3cfb9
Steve Hall <hall.steven.g@gmail.com>
2019-06-02 15:06:24 -0700

    update demo

--- demos/CurlNoise/simulation/index.js
+++ demos/CurlNoise/simulation/index.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
 import { timer } from 'd3-timer'
 import { useWebGLContext, useCanvasSize } from '@react-vertex/core'
-// import config from './config'
+import { SIM_RESOLUTION, DYE_RESOLUTION } from './config'
 import usePointers from './usePointers'
 import useSplatProgram from './useSplatProgram'
+import useResolution from './useResolution'
 
 export default function useSimulation() {
   const { width, height } = useCanvasSize()
@@ -13,9 +14,12 @@ export default function useSimulation() {
 
   const splat = useSplatProgram()
 
+  const simSize = useResolution(SIM_RESOLUTION, width, height)
+  const dyeSize = useResolution(DYE_RESOLUTION, width, height)
+
   useEffect(() => {
     function updateSplat(x, y, dx, dy, color) {
-      // gl.viewport(0, 0, simWidth, simHeight)
+      gl.viewport(0, 0, ...simSize)
 
       gl.useProgram(splat.program)
       // gl.uniform1i(splat.uniforms.uTarget, velocity.read.attach(0))
@@ -24,11 +28,12 @@ export default function useSimulation() {
       // blit(velocity.write.fbo)
       // velocity.swap()
 
-      // gl.viewport(0, 0, dyeWidth, dyeHeight)
+      gl.viewport(0, 0, ...dyeSize)
       // gl.uniform1i(splat.uniforms.uTarget, density.read.attach(0))
       gl.uniform3f(splat.uniforms.color, color.r, color.g, color.b)
       // blit(density.write.fbo)
       // density.swap()
+      console.log(dx, dy)
     }
 
     function input() {
@@ -47,5 +52,5 @@ export default function useSimulation() {
     })
 
     return () => timerLoop.stop()
-  }, [gl, pointers, width, height, splat])
+  }, [gl, simSize, dyeSize, pointers, width, height, splat])
 }
---
+++ demos/CurlNoise/simulation/useResolution.js
@@ -0,0 +1,22 @@
+import { useMemo } from 'react'
+
+export default function useResolution(size, width, height) {
+  const memoized = useMemo(() => {
+    let aspectRatio = width / height
+    
+    if (aspectRatio < 1) {
+      aspectRatio = 1.0 / aspectRatio
+    }
+
+    const max = Math.round(size, width, height * aspectRatio)
+    const min = Math.round(size, width, height)
+
+    if (width > height) {
+      return [max, min]
+    } else {
+      return [min, max]
+    }
+  }, [size, width, height])
+
+  return memoized
+}
