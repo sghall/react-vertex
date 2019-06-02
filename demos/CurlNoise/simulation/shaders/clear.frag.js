25c125846ba69f4317ba2fb8fd3ae67a0660ac74
Steve Hall <hall.steven.g@gmail.com>
2019-06-02 13:16:47 -0700

    add shaders

---
+++ demos/CurlNoise/simulation/shaders/clear.frag.js
@@ -0,0 +1,12 @@
+export default `
+  precision mediump float;
+  precision mediump sampler2D;
+
+  varying highp vec2 vUv;
+  uniform sampler2D uTexture;
+  uniform float value;
+
+  void main () {
+    gl_FragColor = value * texture2D(uTexture, vUv);
+  }
+`
---
+++ demos/CurlNoise/simulation/shaders/pressure.frag.js
@@ -0,0 +1,30 @@
+export default `
+  precision mediump float;
+  precision mediump sampler2D;
+
+  varying highp vec2 vUv;
+  varying highp vec2 vL;
+  varying highp vec2 vR;
+  varying highp vec2 vT;
+  varying highp vec2 vB;
+  uniform sampler2D uPressure;
+  uniform sampler2D uDivergence;
+
+  vec2 boundary (vec2 uv) {
+    return uv;
+    // uncomment if you use wrap or repeat texture mode
+    // uv = min(max(uv, 0.0), 1.0);
+    // return uv;
+  }
+
+  void main () {
+    float L = texture2D(uPressure, boundary(vL)).x;
+    float R = texture2D(uPressure, boundary(vR)).x;
+    float T = texture2D(uPressure, boundary(vT)).x;
+    float B = texture2D(uPressure, boundary(vB)).x;
+    float C = texture2D(uPressure, vUv).x;
+    float divergence = texture2D(uDivergence, vUv).x;
+    float pressure = (L + R + B + T - divergence) * 0.25;
+    gl_FragColor = vec4(pressure, 0.0, 0.0, 1.0);
+  }
+`
