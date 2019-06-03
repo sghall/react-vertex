aa7411e64c23cb5bdcd73dad0d4c54012a5553fe
Steve Hall <hall.steven.g@gmail.com>
2019-06-02 22:29:49 -0700

    update demo

---
+++ demos/CurlNoise/simulation/useAdvectionProgram/index.js
@@ -0,0 +1,20 @@
+import { useMemo } from 'react'
+import {
+  useWebGLContext,
+  useProgram,
+  useProgramUniforms,
+} from '@react-vertex/core'
+import vert from '../shaders/base.vert'
+import frag from './vorticity.frag'
+
+export default function useVorticityProgram() {
+  const gl = useWebGLContext()
+  const program = useProgram(gl, vert, frag)
+  const uniforms = useProgramUniforms(gl, program)
+
+  const vorticity = useMemo(() => {
+    return { program, uniforms }
+  }, [program, uniforms])
+
+  return vorticity
+}
