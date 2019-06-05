fe881dd0af1d660b17d098c03113804d83bddea1
Steve Hall <hall.steven.g@gmail.com>
2019-06-05 00:00:27 -0700

    updates to demo

--- demos/FlockingBirds/index.js
+++ demos/FlockingBirds/index.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import { Canvas } from '@react-vertex/core'
 import { useMeasure } from '@react-vertex/dom-hooks'
 import { convertHex } from '@react-vertex/color-hooks'
@@ -36,6 +37,7 @@ function FlockingBirds() {
         >
           <Scene />
         </Canvas>
+        <Typography>* Zooming is enabled on this demo.</Typography>
       </div>
     </DemoWrapper>
   )
