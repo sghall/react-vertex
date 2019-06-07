b4d3268ac3295243ff5d0b77dbebc8336cb378f7
Steve Hall <hall.steven.g@gmail.com>
2019-06-07 12:48:27 -0700

    clean up

--- demos/FluidSimulation/customHooks/useFormats.js
+++ demos/FluidSimulation/customHooks/useFormats.js
@@ -2,23 +2,20 @@ import { useMemo } from 'react'
 import { useWebGLVersion } from '@react-vertex/core'
 
 function supportRenderTextureFormat(gl, internalFormat, format, type) {
-  let texture = gl.createTexture()
+  const texture = gl.createTexture()
   gl.bindTexture(gl.TEXTURE_2D, texture)
+
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
   gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, 4, 4, 0, format, type, null)
 
-  let fbo = gl.createFramebuffer()
+  const fbo = gl.createFramebuffer()
+  const attach = gl.COLOR_ATTACHMENT0
+
   gl.bindFramebuffer(gl.FRAMEBUFFER, fbo)
-  gl.framebufferTexture2D(
-    gl.FRAMEBUFFER,
-    gl.COLOR_ATTACHMENT0,
-    gl.TEXTURE_2D,
-    texture,
-    0,
-  )
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, attach, gl.TEXTURE_2D, texture, 0)
 
   const status = gl.checkFramebufferStatus(gl.FRAMEBUFFER)
 
@@ -41,10 +38,7 @@ function getSupportedFormat(gl, internalFormat, format, type) {
     }
   }
 
-  return {
-    internalFormat,
-    format,
-  }
+  return { internalFormat, format }
 }
 
 export default function useFormats(gl) {
