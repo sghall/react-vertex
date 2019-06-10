566f1e1c9e967ae25b14462edd5e7de61fc51b4d
Steve Hall <hall.steven.g@gmail.com>
2019-06-10 07:59:39 -0700

    update demo

--- demos/FlockingBirds/useCompute/velocity.frag.js
+++ demos/FlockingBirds/useCompute/velocity.frag.js
@@ -1,5 +1,6 @@
 export default `
   precision <<FLOAT_PRECISION>> float;
+  precision highp sampler2D;
 
   // shader adapted from the awesome THREE.js demo:
   // https://github.com/mrdoob/three.js/blob/master/examples/webgl_gpgpu_birds.html
