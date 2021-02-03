13e30f130e1d7c56bbe12f6c807b384f79987301
Steven Hall <sghall@protonmail.com>
2021-02-03 07:04:26 -0700

    clean up

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>React Vertex</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
