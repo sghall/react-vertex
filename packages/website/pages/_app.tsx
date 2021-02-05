e06f662332a4b311653f606956919d42659beb64
Steven Hall <sghall@protonmail.com>
2021-02-05 10:57:50 -0700

    configure ts for next

--- packages/website/pages/_app.tsx
+++ packages/website/pages/_app.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Head from 'next/head'
+import { AppProps } from 'next/app'
 import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
 import AppFrame from '../components/AppFrame'
 import { theme } from '../config'
 
-export default function MyApp(props) {
+export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles) {
+      // @ts-ignore
       jssStyles.parentElement.removeChild(jssStyles)
     }
   }, [])
@@ -36,9 +37,4 @@ export default function MyApp(props) {
       </ThemeProvider>
     </React.Fragment>
   )
-}
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
 }
