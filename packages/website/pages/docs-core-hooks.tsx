90e0825ba37951e52e5da593da19f2ad4d8dc61e
Steven Hall <sghall@protonmail.com>
2021-02-23 07:10:00 -0700

    fix core docs

---
+++ packages/website/pages/docs-core-hooks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import MarkdownElement from '../components/MarkdownElement'
+import docs from '../../core/README.md'
+
+function CoreHooks() {
+  return <MarkdownElement docs={docs} />
+}
+
+export default CoreHooks
